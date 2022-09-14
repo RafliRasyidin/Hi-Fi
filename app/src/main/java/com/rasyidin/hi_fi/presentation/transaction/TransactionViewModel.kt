bb1683124941541a6a5c7fdb536eea4d0e708393
RafliRasyidin <raflirasyidin20@gmail.com>
2022-09-14 10:09:07 +0700

    add function to get source balance

--- app/src/main/java/com/rasyidin/hi_fi/presentation/transaction/TransactionViewModel.kt
+++ app/src/main/java/com/rasyidin/hi_fi/presentation/transaction/TransactionViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.rasyidin.hi_fi.domain.ResultState
 import com.rasyidin.hi_fi.domain.idle
+import com.rasyidin.hi_fi.domain.model.balance.SourceBalance
 import com.rasyidin.hi_fi.domain.model.transaction.Transaction
 import com.rasyidin.hi_fi.domain.usecase.transaction.UseCaseTransaction
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -13,35 +14,46 @@ import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class TransactionViewModel @Inject constructor(private val useCaseTransaction: UseCaseTransaction) :
+class TransactionViewModel @Inject constructor(private val useCase: UseCaseTransaction) :
     ViewModel() {
 
     private val _transactions: MutableStateFlow<ResultState<List<Transaction>>> = idle()
     val transaction: StateFlow<ResultState<List<Transaction>>> get() = _transactions
 
+    private val _sourceBalance: MutableStateFlow<ResultState<List<SourceBalance>>> = idle()
+    val sourceBalance: StateFlow<ResultState<List<SourceBalance>>> get() = _sourceBalance
+
     fun getTransactions() {
         viewModelScope.launch {
-            useCaseTransaction.getTransactions().collect { resultState ->
+            useCase.getTransactions().collect { resultState ->
                 _transactions.value = resultState
+            }
+        }
+    }
+
+    fun getSourceBalance() {
+        viewModelScope.launch {
+            useCase.getSourceBalance().collect { resultState ->
+                _sourceBalance.value = resultState
             }
         }
     }
 
     fun addTransaction(transaction: Transaction) {
         viewModelScope.launch {
-            useCaseTransaction.addTransaction(transaction)
+            useCase.addTransaction(transaction)
         }
     }
 
     fun editTransaction(transaction: Transaction) {
         viewModelScope.launch {
-            useCaseTransaction.editTransaction(transaction)
+            useCase.editTransaction(transaction)
         }
     }
 
     fun deleteTransaction(transaction: Transaction) {
         viewModelScope.launch {
-            useCaseTransaction.deleteTransaction(transaction)
+            useCase.deleteTransaction(transaction)
         }
     }
 }
