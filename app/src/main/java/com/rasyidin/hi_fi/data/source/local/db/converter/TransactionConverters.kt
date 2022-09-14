47c28ebc4b1828c8e4870f06beb1eddc5ebb939c
RafliRasyidin <raflirasyidin20@gmail.com>
2022-09-14 10:08:22 +0700

    optimize import

--- app/src/main/java/com/rasyidin/hi_fi/data/source/local/db/converter/TransactionConverters.kt
+++ app/src/main/java/com/rasyidin/hi_fi/data/source/local/db/converter/TransactionConverters.kt
@@ -3,7 +3,6 @@ package com.rasyidin.hi_fi.data.source.local.db.converter
 import androidx.room.TypeConverter
 import com.rasyidin.hi_fi.domain.model.balance.TransactionCategorize
 import com.rasyidin.hi_fi.domain.model.balance.TransactionCategorize.*
-import com.rasyidin.hi_fi.domain.model.transaction.Transaction
 
 class TransactionConverters {
 
