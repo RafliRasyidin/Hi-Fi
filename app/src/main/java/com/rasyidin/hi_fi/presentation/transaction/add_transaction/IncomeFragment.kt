2746038af4d95ea62742b7fc110ad7182ca0e738
RafliRasyidin <raflirasyidin20@gmail.com>
2022-09-21 15:33:15 +0700

    create fragment for view pager

---
+++ app/src/main/java/com/rasyidin/hi_fi/presentation/transaction/add_transaction/IncomeFragment.kt
@@ -0,0 +1,15 @@
+package com.rasyidin.hi_fi.presentation.transaction.add_transaction
+
+import android.os.Bundle
+import android.view.View
+import com.rasyidin.hi_fi.databinding.FragmentIncomeBinding
+import com.rasyidin.hi_fi.presentation.component.FragmentBinding
+
+class IncomeFragment : FragmentBinding<FragmentIncomeBinding>(FragmentIncomeBinding::inflate) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+}
---
+++ app/src/main/java/com/rasyidin/hi_fi/presentation/transaction/add_transaction/TransferFragment.kt
@@ -0,0 +1,16 @@
+package com.rasyidin.hi_fi.presentation.transaction.add_transaction
+
+import android.os.Bundle
+import android.view.View
+import com.rasyidin.hi_fi.databinding.FragmentTransferBinding
+import com.rasyidin.hi_fi.presentation.component.FragmentBinding
+
+class TransferFragment :
+    FragmentBinding<FragmentTransferBinding>(FragmentTransferBinding::inflate) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+}
